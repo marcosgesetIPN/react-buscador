d9e8798ff37ce48c038f5d655791505dd746988f
Geset <marcosgeset@outlook.com>
2020-12-03 17:36:48 -0600

    ya con el docs

---
+++ docs/precache-manifest.6c4ce7778bcfcc65045c69568bb1d5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5242b154b302ae9704e46118831e212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13754f2422b873122db9",
+    "url": "/static/css/main.97ea4da2.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13754f2422b873122db9",
+    "url": "/static/js/main.b2408990.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
